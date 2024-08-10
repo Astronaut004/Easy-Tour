069f4511822b9bd7ebb1abdb96324b9f1003c6d6
Astronaut004 <ac4548817@gmail.com>
2024-08-11 00:54:48 +0530

    contact done

--- AdminDashboard/pages/contact.php
+++ AdminDashboard/pages/contact.php
@@ -1,3 +1,16 @@
+<?php
+  session_start();
+
+  if(!isset($_SESSION['username']) || !isset($_SESSION['role'])) {
+    header("Location: ../Entry/login.php");
+    exit();
+}
+if($_SESSION['role'] != "Admin") {
+    header("Location: ../UserPanel");
+    exit();
+  }
+  ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
