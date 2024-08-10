eaa96b03bb7abb8301992c8cee450c82bc5f2f05
Astronaut004 <ac4548817@gmail.com>
2024-08-11 00:48:33 +0530

    Delete the request for city

---
+++ AdminDashboard/City/DeleteRequestCity.php
@@ -0,0 +1,38 @@
+<?php
+  session_start();
+
+  if(!isset($_SESSION['username']) || !isset($_SESSION['role'])) {
+    header("Location: ../Entry/login.php");
+    exit();
+}
+if($_SESSION['role'] != "Admin") {
+    header("Location: ../UserPanel");
+    exit();
+  }
+  ?>
+
+
+
+<?php
+include "../../connection.php";
+        $name = $_REQUEST['sname'];
+
+        $sql = "DELETE FROM request_city_tb WHERE city_name=?";
+        $stmt = $con->prepare($sql);
+        $stmt->bind_param("s", $name);
+        $stmt->execute();
+        if (mysqli_affected_rows($con) > 0) {
+            echo " 
+                <script>
+                window.open('../Dashboard.php','_self');
+                </script>
+            ";
+        } else {
+            echo " 
+            <script>
+            window.open('../Dashboard.php','_self');
+            </script>
+        ";
+        }
+
+?>
